146f3cb34d6cad9597b7ec3798865c4a0d110b2b
Joshua Bidwell <jjbidwell262@gmail.com>
2021-03-03 14:34:21 -0800

    Moved routes to server.js file

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const path = require("path");
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send('Hello friend you are here')
+})
+
 app.listen(3000, () => console.log("server listening on Port 3000"))
