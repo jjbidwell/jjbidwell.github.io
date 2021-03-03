48e0b24391413c9ce75313f8283aa0b58181097f
Joshua Bidwell <jjbidwell262@gmail.com>
2021-03-03 14:40:14 -0800

    Moved assets to public folder and added express path

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+app.use(express.static(path.join("public")))
 
 app.get("/", (req, res) => {
-    res.send('Hello friend you are here')
+    res.sendFile("index.html");
 })
 
 app.listen(3000, () => console.log("server listening on Port 3000"))
